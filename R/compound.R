2fef7f2ed3ffbb648a91c0eb780394b3226997d9
ErikavW <vanwijk.erika@gmail.com>
2015-02-18 10:03:57 +0100

    Correct error message!

--- R/compound.R
+++ R/compound.R
@@ -33,14 +33,12 @@ compound<-function(y,alpha,theta,beta,response,Sim,D,M,identification=1,draw.sta
   library("mvtnorm")     # Multivariate normale verdeling
   library("MCMCpack")    # Inverse Wishart verdeling
   
-  if(is.matrix(y)){
+  if(!is.matrix(y)){
     print("The dataframe included is not a matrix")
   }
   
   N<-nrow(y)
   K<-ncol(y)
-  
-  if (N==NULL)
   
   # beta <- CLEAN_itempar(y,beta)
   if (draw.start == T){
