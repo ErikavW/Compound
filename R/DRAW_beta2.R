690d6ca82ff1fcf9a4a0a2ca9e63976d04bc089f
ErikavW <vanwijk.erika@gmail.com>
2015-02-20 11:13:03 +0100

    Finishing up

--- R/DRAW_beta2.R
+++ R/DRAW_beta2.R
@@ -1,4 +1,4 @@
-DRAW_beta2 <- function(y,z,response,alpha,theta,beta,M,sd.MH=0.05, var.p,u.p){
+DRAW_beta2 <- function(y,z,response,alpha,theta,beta,M,sd.MH=0.05,var.p,u.p){
   b<-beta
   K<-nrow(beta)
   N<-nrow(y)
@@ -8,54 +8,58 @@ DRAW_beta2 <- function(y,z,response,alpha,theta,beta,M,sd.MH=0.05, var.p,u.p){
       temp1<-1
       temp2<-1
       
-      if(j ==1){
-        b[i,j]<-rtnorm(1,beta[i,j],sd.MH,right=beta[i,(j+1)]) #Trekking van b
-      }
-      if(j ==M[i]){
-        b[i,j]<-rtnorm(1,beta[i,j],sd.MH,left=b[i,(j-1)]) #Trekking van b
-      }
-        
-      for (j in 2:(M[i]-1)){
-        b[i,j]<-rtnorm(1,beta[i,j],sd.MH,left=b[i,(j-1)],right=beta[i,(j+1)]) #Trekking van b
+      for (j in 1:M[i]){
+        if(j ==1){
+           b[i,1]<-rtnorm(1,beta[i,1],sd.MH,right=beta[i,(2)]) #Trekking van b
+        }else{
+          if(j ==M[i]){
+            b[i,M[i]]<-rtnorm(1,beta[i,M[i]],sd.MH,left=b[i,(M[i]-1)]) #Trekking van b
+          }else{
+            b[i,j]<-rtnorm(1,beta[i,j],sd.MH,left=b[i,(j-1)],right=beta[i,(j+1)]) #Trekking van b
+          }
+        }
       }
       
       for (n in 1:N){
         ee<- y[n,i]
         true <- alpha[i]*theta[n]
-        if(y[n,i]==1){
-          Ab<-1-pnorm(true-b[i,ee])
-          Bb<-1-pnorm(true-beta[i,ee])
-        }else{
-          if(y[n,i]==M[i]){
-            Ab<-pnorm(true-b[i,ee-1])
-            Bb<-pnorm(true-beta[i,ee-1])
+        if (y[n,i]!=0){
+          if(y[n,i]==1){
+            Ab<-1-pnorm(true-b[i,ee])
+            Bb<-1-pnorm(true-beta[i,ee])
           }else{
-            Ab<-pnorm(true-b[i,ee-1])-pnorm(true-b[i,ee])
-            Bb<-pnorm(true-beta[i,ee-1])-pnorm(true-beta[i,ee])
-          }          
+            if(y[n,i]==M[i]){
+              Ab<-pnorm(true-b[i,ee-1])
+              Bb<-pnorm(true-beta[i,ee-1])
+            }else{
+              Ab<-pnorm(true-b[i,ee-1])-pnorm(true-b[i,ee])
+              Bb<-pnorm(true-beta[i,ee-1])-pnorm(true-beta[i,ee])
+            }          
+          }
         }
-        temp1<-temp1*(Ab/Bb)        
+        temp1<-temp1*(Ab/Bb)   
       }
       for (n in 1:N){
-        if(j==1){
-          Ab<-pnorm((beta[i,(j+1)]-beta[i,j]),sd.MH)-1
-          Bb<-pnorm((b[i,(j+1)]-b[i,j]),sd.MH)-1
+        for(j in 1:M[i]){
+          if(j==1){
+            Ab<-pnorm((beta[i,2]-beta[i,1]),sd.MH)
+            Bb<-pnorm((b[i,2]-b[i,1]),sd.MH)
+          }else{
+            if(j==M[i]){
+              Ab<-1-pnorm((b[i,(M[i]-1)]-beta[i,M[i]]),sd.MH)
+              Bb<-1-pnorm((beta[i,(M[i]-1)]-b[i,M[i]]),sd.MH)
+            }else{
+              Ab<-pnorm((beta[i,(j+1)]-beta[i,j]),sd.MH)-pnorm((b[i,(j-1)]-beta[i,j]),sd.MH)
+              Bb<-pnorm((b[i,(j+1)]-b[i,j]),sd.MH)-pnorm((beta[i,(j-1)]-b[i,j]),sd.MH)
+            }
+          }
         }
-        if(j==M[i]){
-          Ab<-0-pnorm((b[i,(j-1)]-beta[i,j]),sd.MH)
-          Bb<-0-pnorm((beta[i,(j-1)]-b[i,j]),sd.MH)
-        }
-        for (j in 2:(M[i]-1)){
-          Ab<-pnorm((beta[i,(j+1)]-beta[i,j]),sd.MH)-pnorm((b[i,(j-1)]-beta[i,j]),sd.MH)
-          Bb<-pnorm((b[i,(j+1)]-b[i,j]),sd.MH)-pnorm((beta[i,(j-1)]-b[i,j]),sd.MH)
-        }          
-      temp2<-temp2*(Ab/Bb)        
-       
+        temp2<-temp2*(Ab/Bb)        
       } 
       R=temp1*temp2
       ru<-runif(1,0,1)
       if (R<=ru){
-        beta[i,1:M[i])]<-b[i,M[i]]
+        beta[i,1:M[i]]<-b[i,M[i]]
       }
     }    
   }else{
--- R/compound.R
+++ R/compound.R
@@ -99,7 +99,7 @@ compound<-function(y,alpha,theta,beta,response,Sim,D,M,identification=1,draw.sta
     
     # Stap 5 Trek itemparameters  
     if(fix.itemparameters==F){
-      beta <- DRAW_beta2(y,z,response,alpha,theta,beta,M,sd.MH=0.01, var.p,u.p)
+      beta <- DRAW_beta2(y,z,response,alpha,theta,beta,M,sd.MH=0.01,var.p,u.p)
       alpha <- DRAW_alpha(z,alpha,theta,var.p,u.p)
       
       # Matrixes met caterpillar voor alpha en beta
@@ -108,9 +108,9 @@ compound<-function(y,alpha,theta,beta,response,Sim,D,M,identification=1,draw.sta
     }
   }
   if (D==1){
-    return(list(alpha=alpha,theta=theta,beta=beta[,2:(max(M)+1)],A=A,B=B))
+    return(list(alpha=alpha,theta=theta,beta=beta[,1:max(M)],A=A,B=B))
   }else{
-    return(list(alpha=alpha,theta=theta,beta=beta[,2:(max(M)+1)],rho=rho,weights=weights,A=A,B=B))    
+    return(list(alpha=alpha,theta=theta,beta=beta[,1:max(M)],rho=rho,weights=weights,A=A,B=B))    
   }
   
 }
